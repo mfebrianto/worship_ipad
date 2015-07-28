c51c2b6930411af990a504eee87fcad6742c24f5
mfebrianto <michaelfebrianto@gmail.com>
2015-07-28 11:45:55 +1000

    Initial Commit

---
+++ Worship/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Worship
+//
+//  Created by michael febrianto on 28/07/2015.
+//  Copyright (c) 2015 f2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Worship/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Worship
+//
+//  Created by michael febrianto on 28/07/2015.
+//  Copyright (c) 2015 f2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
